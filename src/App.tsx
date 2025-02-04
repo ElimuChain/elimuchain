7344c2dae75b5a0e6587debd78c6afcce3dff0c8
khahussein <kherld.hussein@gmail.com>
2025-02-04 03:56:52 +0300

    core(app): Update Core App imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import GetStarted from "./pages/GetStarted";
 import SignIn from "./pages/SignIn";
@@ -12,19 +17,24 @@ import PartnersPage from "./pages/PartnersPage";
 
 function App() {
   return (
-    <Router future={{ v7_relativeSplatPath: true }}>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/careers" element={<CareersPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/documentation" element={<DocumentationPage />} />
-        <Route path="/support" element={<SupportPage />} />
-        <Route path="/partners" element={<PartnersPage />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/get-started/*" element={<GetStarted />} />
-        <Route path="/dashboard/*" element={<Dashboard />} />
-      </Routes>
+    <Router future={{ v7_startTransition: true }}>
+      {" "}
+      {/* Enable the flag for relative splat path */}
+      <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50 dark:from-gray-900 dark:to-gray-800">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/careers" element={<CareersPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/documentation" element={<DocumentationPage />} />
+          <Route path="/support" element={<SupportPage />} />
+          <Route path="/partners" element={<PartnersPage />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/get-started/*" element={<GetStarted />} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
