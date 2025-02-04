61a86a361afea50c43ff4cc85b1061f3758b10b4
khahussein <kherld.hussein@gmail.com>
2025-02-04 03:18:31 +0300

    feat(signin): Add sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import GetStarted from "./pages/GetStarted";
+import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import AboutPage from "./pages/AboutPage";
 import CareersPage from "./pages/CareersPage";
@@ -11,7 +12,7 @@ import PartnersPage from "./pages/PartnersPage";
 
 function App() {
   return (
-    <Router>
+    <Router future={{ v7_relativeSplatPath: true }}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
@@ -20,6 +21,7 @@ function App() {
         <Route path="/documentation" element={<DocumentationPage />} />
         <Route path="/support" element={<SupportPage />} />
         <Route path="/partners" element={<PartnersPage />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/get-started/*" element={<GetStarted />} />
         <Route path="/dashboard/*" element={<Dashboard />} />
       </Routes>
